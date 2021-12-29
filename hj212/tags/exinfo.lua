765200c7864a5e61fb042e1cea8e9f128d82bd7d
Dirk Chang <dirk@kooiot.com>
2021-12-29 12:02:57 +0800

    added HeBei tags

--- hj212/tags/exinfo.lua
+++ hj212/tags/exinfo.lua
@@ -1,25 +1,34 @@
 local EX_TAGS = {}
 local logger = require 'hj212.logger'
 
-return {
-	add = function(id, desc, format, org_id, unit, cou_unit)
-		--print(id, desc, format, org_id, unit, cou_unit)
-		assert(id)
-		assert(desc)
-		if EX_TAGS[id] then
-			logger.error("Duplicated id found", id)
-			return
-		end
-		EX_TAGS[id] = {
-			id = id,
-			desc = desc,
-			format = format,
-			org_id = org_id,
-			unit = unit,
-			cou_unit = cou_unit
-		}
-	end,
-	find = function(id)
-		return EX_TAGS[id]
+local _M = {}
+
+_M.add = function(id, desc, format, org_id, unit, cou_unit)
+	--print(id, desc, format, org_id, unit, cou_unit)
+	assert(id)
+	assert(desc)
+	if EX_TAGS[id] then
+		logger.error("Duplicated id found", id)
+		return
 	end
-}
+	EX_TAGS[id] = {
+		id = id,
+		desc = desc,
+		format = format,
+		org_id = org_id,
+		unit = unit,
+		cou_unit = cou_unit
+	}
+end
+
+_M.load = function(cate)
+	local info_list = require 'hj212.tags.'..cate..'.info'
+	for k, v in pairs(info_list) do
+		_M.add(k, v.desc, v.format, v.org_id, v.unit, v.cou_unit)
+	end
+end
+
+_M.find = function(id)
+	return EX_TAGS[id]
+end
+
---
+++ hj212/tags/hebei/info.lua
@@ -0,0 +1,406 @@
+return {
+	-- Air
+	i12007 = {
+		desc = '气态分析仪工作状态',
+		format = 'N2',
+	},
+	i12008 = {
+		desc = '气态分析仪报警状态',
+		format = 'N2',
+	},
+	i12009 = {
+		desc = '气态分析仪报警详情',
+		format = 'N2',
+	},
+	--- Water
+	i12101 = {
+		desc = '工作状态',
+		format = 'N2'
+	},
+	i12102 = {
+		desc = '报警状态',
+		format = 'N2',
+	},
+	i12103 = {
+		desc = '报警详情',
+		format = 'N2',
+	},
+	--- Air
+	i13010 =  {
+		desc = '量程校准漂移',
+		format = 'N2.1',
+		unit = '%',
+	},
+	i13011 = {
+		desc = '测量原始值',
+	},
+	i13012 = {
+		desc = '稀释比',
+	},
+	i13013 = {
+		desc = '量程',
+		unit = 'mg/m3',
+	},
+	i13014 = {
+		desc = '污染度(浊度法)',
+	},
+	i13015 = {
+		desc = '平滑时间',
+		unit = '分钟',
+	},
+	i13016 = {
+		desc = '空白粒子数(贝它射线法)'
+	},
+	i13017 = {
+		desc = '采样后粒子数(贝它射线法)'
+	},
+	i13018 = {
+		desc = '采样总流量(贝它射线法)'
+	},
+	i13019 = {
+		desc = '质量比(贝它射线法)'
+	},
+	i13020 = {
+		desc = '采样流量(贝它射线法)'
+	},
+	i13021 = {
+		desc = '点校准时间',
+		format = 'YYYYMMDDHHMMSS',
+	},
+	i13022 = {
+		desc = '零点校准系数', -- 截距
+	},
+	i13023 = {
+		desc = '零点校准浓度', -- 校零的零气浓度
+		unit = 'mg/m3'
+	},
+	i13024 = {
+		desc = '零点校准原始值',
+	},
+	i13025 = {
+		desc = '零点校准漂移',
+		format = 'N2.1',
+		unit = '%',
+	},
+	i13026 = {
+		desc = '量程校准系数', -- 斜率
+	},
+	i13027 = {
+		desc = '量程校准时间',
+		format = 'YYYYMMDDHHMMSS',
+	},
+	i13028 = {
+		desc = '量程校准浓度', -- 标气浓度
+	},
+	i13029 = {
+		desc = '量程校准原始值',
+	},
+	-- Water
+	i13101 = {
+		desc = '零点校准时间',
+		format = 'YYYYMMDDHHMMSS',
+	},
+	--[[ use i13007
+	i13102 = {
+		desc = '截距',
+		unit = 'L',
+		format = 'N5.2',
+	},
+	]]--
+	i13103 = {
+		desc = '截距1',
+		unit = 'L',
+		format = 'N5.2',
+	},
+	i13104 = {
+		desc = '零点校准浓度',
+	},
+	i13105 = {
+		desc = '零点校准原始值',
+	},
+	i13106 = {
+		desc = '零点校准漂移',
+		format = 'N2.1',
+		unit = '%',
+	},
+	i13107 = {
+		desc = '量程校准时间',
+		format = 'YYYYMMDDHHMMSS',
+	},
+	i13108 = {
+		desc = '量程校准浓度',
+	},
+	i13109 = {
+		desc = '量程校准浓度1',
+	},
+	i13110 = {
+		desc = '量程校准原始值',
+	},
+	i13111 = {
+		desc = '量程校准原始值1',
+	},
+	i13112 =  {
+		desc = '量程校准漂移',
+		format = 'N2.1',
+		unit = '%',
+	},
+	i13113 =  {
+		desc = '量程校准漂移1',
+		format = 'N2.1',
+		unit = '%',
+	},
+	--[[ use i13003
+	i13114 = {
+		desc = '测量间隔',
+		unit = '分钟',
+	},
+	]]--
+	i13115 = {
+		desc = '测量原始值',
+	},
+	i13116 = {
+		desc = '当前量程',
+	},
+	--[[ use i13008
+	i13117 = {
+		desc = '斜率',
+		format = 'N5.2',
+	},
+	]]--
+	i13118 = {
+		desc = '斜率1',
+		format = 'N5.2',
+	},
+	i13119 = {
+		desc = '校正因子(修正斜率)'
+	},
+	i13120 = {
+		desc = '修正因子(修正截距)'
+	},
+	--[[ use i13004
+	i13121 = {
+		desc = '消解时长',
+		unit = '分钟'
+	},
+	]]--
+	i13122 = {
+		desc = '消解温度',
+		unit = 'C',
+	},
+	i13123 = {
+		desc = '冷却时长',
+		unit = 'min',
+	},
+	i13124 = {
+		desc = '硫酸亚铁铵标定时间',
+		unit = 'min',
+	},
+	i13125 = {
+		desc = '硫酸亚铁铵浓度',
+		unit = 'g/L',
+	},
+	i13126 = {
+		desc = '空白硫酸亚铁铵标准溶液体积',
+		unit = 'L',
+	},
+	i13127 = {
+		desc = '重铬酸钾浓度',
+		unit = 'g/L',
+	},
+	i13128 = {
+		desc = '标样核查时间',
+		format = 'YYYYMMDDHHMMSS'
+	},
+	i13129 = {
+		desc = '标样核查偏差百分比',
+		format = 'N2.1',
+		unit = '%',
+	},
+	i13130 = {
+		desc = '标样核查标液浓度',
+		unit = 'mg/L',
+	},
+	-- STATE
+	i22001 = {
+		desc = '数采仪工作状态',
+		format = 'N2',
+	},
+	i22002 = {
+		desc = '数采仪通讯状态',
+		format = 'N2',
+	},
+	i22003 = {
+		desc = '数采仪报警详情',
+		format = 'N2',
+	},
+	i22004 = {
+		desc = '数采仪通讯状态',
+		format = 'N2',
+	},
+	i22005 = {
+		desc = '数采仪报警详情',
+		format = 'N2',
+	},
+	i52001 = {
+		desc = '视频报警',
+		format = 'N2',
+	},
+
+	-- INFO
+	i23002 = {
+		desc = '基准氧含量',
+		format = 'N2.2',
+	},
+	i23003 = {
+		desc = '速度场系数',
+		format = 'N2.4',
+	},
+	i23004 = {
+		desc = '皮托管系数',
+		format = 'N2.2',
+	},
+	i23005 = {
+		desc = '烟气湿度量程',
+		format = 'N5.2',
+		unit = '%',
+	},
+	i23006 = {
+		desc = '烟气温度量程上限',
+		format = 'N5.2',
+		unit = 'C'
+	},
+	i23007 = {
+		desc = '烟气温度量程下限',
+		format = 'N5.2',
+		unit = 'C'
+	},
+	i23008 = {
+		desc = '烟气压力量程上限',
+		format = 'N5.2',
+		unit = 'KP',
+	},
+	i23009 = {
+		desc = '烟气压力量程下限',
+		format = 'N5.2',
+		unit = 'KP',
+	},
+	i23010 = {
+		desc = '烟气流速量程',
+		format = 'N5.2',
+		unit = 'm/s',
+	},
+	i23011 = {
+		desc = '本地大气压（河北）',
+		format = 'N5.2',
+		unit = 'Pa'
+	},
+
+	--
+	i3310A = {
+		desc = '门禁编码',
+		format = 'C30'
+	},
+	i3310B = {
+		desc = '开门方式',
+		format = 'N2'
+	},
+	i3310C = {
+		desc = '开门类型',
+		format = 'N2'
+	},
+	i3310D = {
+		desc = '开门编码',
+		format = 'C30'
+	},
+	i3310E = {
+		desc = '开门时间',
+		format = 'YYYYMMDDHHMMSS'
+	},
+	i3310F = {
+		desc = '门状态',
+		format = 'N2'
+	},
+	i3310G = {
+		desc = 'IC卡卡号',
+		format = 'C32'
+	},
+	i3310H = {
+		desc = '人员图像编号',
+		format = 'C12'
+	},
+	i3310I = {
+		desc = '人员图像地址',
+		format = 'C100'
+	},
+	i3310J = {
+		desc = '人员姓名',
+		format = 'C10'
+	},
+
+	-- Door history
+	i41001 = {
+		desc = '开关门记录',
+		format = 'C890',
+	},
+	i41002 = {
+		desc = '断电记录',
+		format = 'C890',
+	},
+
+	-- Sample
+	i42001 = {
+		desc = '设备状态',
+		format = 'N2',
+	},
+	i42002 = {
+		desc = '系统时间',
+		format = 'YYYYMMDDHHMMSS',
+	},
+	i42003 = {
+		desc = '门禁状态',
+		format = 'N2',
+	},
+	i42004 = {
+		desc = '混匀桶A桶工作状态',
+		format = 'N2',
+	},
+	i42005 = {
+		desc = '混匀桶B桶工作状态',
+		format = 'N2',
+	},
+	i42006 = {
+		desc = '异常状态',
+		format = 'N2',
+	},
+
+	-- Water Sample
+	i43001 = {
+		desc = '采样模式',
+		format = 'N2',
+	},
+	i43002 = {
+		desc = '留样模式',
+		format = 'N2',
+	},
+	i43003 = {
+		desc = '采样量',
+		format = 'N4',
+		unit = 'mL',
+	},
+	i43004 = {
+		desc = '采样间隔',
+		format = 'N4',
+		unit = 'min',
+	},
+	i43005 = {
+		desc = '留样量',
+		format = 'N4',
+		unit = 'mL',
+	},
+	i43006 = {
+		desc = '留样瓶编号',
+		format = 'N2',
+	},
+}
+
+

aa592b57f3cc5dd14d59490f1e6bd961ce787381
Dirk Chang <dirk@kooiot.com>
2022-01-10 16:21:05 +0800

    calculate the maintain/error/calibration sample by default

--- hj212/calc/helper.lua
+++ hj212/calc/helper.lua
@@ -1,6 +1,20 @@
 local types = require 'hj212.types'
 
-local _M = {}
+local _M = {
+	_calc_flags = {
+		[types.FLAG.Maintain] = true,
+		[types.FLAG.Error] = true,
+		[types.FLAG.Calibration] = true,
+	}
+}
+
+function _M.add_calc_flag(flag)
+	_M._calc_flags[flag] = true
+end
+
+function _M.remove_calc_flag(flag)
+	_M._calc_flags[flag] = nil
+end
 
 function _M.flag_can_calc(flag)
 	if flag == nil then
@@ -9,6 +23,11 @@ function _M.flag_can_calc(flag)
 	if flag == types.FLAG.Normal or flag == types.FLAG.Overproof then
 		return true
 	end
+
+	if _M._calc_flags[flag] then
+		return true
+	end
+
 	return false
 end
 
